544573b080f5f1d1fa4f2db7e052cfa6e1bbebaa
paulanguedes <paulanguedes@gmail.com>
2021-04-13 21:02:25 -0300

    excluding past time in appointments

--- src/modules/appointments/services/ListProvidersDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProvidersDayAvailabilityService.spec.ts
@@ -13,12 +13,16 @@ describe('ListProvidersDayAvailability', () => {
   it('should be able to list the available hours in a day of a provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2021, 4, 20, 8, 0, 0),
+      date: new Date(2021, 4, 20, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2021, 4, 20, 10, 0, 0),
+      date: new Date(2021, 4, 20, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2021, 4, 20, 11).getTime();
     });
 
     const availability = await listProvidersDayAvailability.execute({
@@ -31,9 +35,13 @@ describe('ListProvidersDayAvailability', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 11, available: true },
+        { hour: 11, available: false },
+        { hour: 12, available: true },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
       ]));
   });
 
--- src/modules/appointments/services/ListProvidersDayAvailabilityService.ts
+++ src/modules/appointments/services/ListProvidersDayAvailabilityService.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
-import { getDate, getDaysInMonth, getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 
 interface IRequest {
   provider_id: string,
@@ -37,14 +37,18 @@ export default class ListProvidersDayAvailabilityService {
       (_, index) => index + startHour
     );
 
+    const currentDate = new Date(Date.now());
+
     const availability = eachHourArray.map(hour => {
       const hasAppointmentInHour = appointments.find(appointment =>
         getHours(appointment.date) === hour,
-      )
+      );
+
+      const compareDate = new Date(year, month - 1, day, hour);
 
       return {
         hour,
-        available: !hasAppointmentInHour
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
       }
     });
 
