349d8b8ef52c95b904948a9a436a3695c2c81fed
paulanguedes <paulanguedes@gmail.com>
2021-04-24 16:24:40 -0300

    Amazon SES configuration

---
+++ src/config/mail.ts
@@ -0,0 +1,22 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    }
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'paula.guedes@customerx.cx',
+      name: 'Paula da GoBarber'
+    }
+  }
+} as IMailConfig;
+
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -5,6 +5,8 @@ import DiskStorageProvider from './StorageProvider/implementations/DiskStoragePr
 
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
+import mailConfig from '@config/mail';
 
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
@@ -21,5 +23,7 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider)
+  mailConfig.driver === 'ethereal' ?
+    container.resolve(EtherealMailProvider) :
+    container.resolve(SESMailProvider)
 );
